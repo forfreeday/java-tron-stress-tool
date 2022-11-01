fc7fabfe9cb4493840b2f27dab5e90e23a5e7817
liukai <forfreeday@163.com>
2022-11-01 16:34:22 +0800

    feat(shell): modify tool name

--- tool/src/main/resources/install.sh
+++ tool/src/main/resources/install.sh
@@ -11,5 +11,5 @@ download() {
   fi
 }
 
-download "https://raw.githubusercontent.com/forfreeday/java-tron-stress-tool/main/tool/src/main/resources/tron-develop.sh" /usr/bin/tron-develop
-chmod +x /usr/bin/tron-develop
+download "https://raw.githubusercontent.com/forfreeday/java-tron-stress-tool/main/tool/src/main/resources/tron-develop.sh" /usr/bin/tron-tool
+chmod +x /usr/bin/tron-tool
