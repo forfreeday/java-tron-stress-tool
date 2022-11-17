cf35d409361c43fe4f2082edb3a6c0bc44578e5d
liukai <forfreeday@163.com>
2022-11-17 19:10:04 +0800

    feat(shell): add download shell

--- tool/src/main/resources/config/liteDatabase.sh
+++ tool/src/main/resources/config/liteDatabase.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
+
+# nohup sh liteDatebase.sh 2>&1 &
 databaseDir=liteDatabase/
 IP="http://47.89.178.46"
-# example: http://47.89.178.46/backup20221117/
 backupDate=`date +%Y%m%d --date="-1 day"`
 url="${IP}/backup${backupDate}/LiteFullNode_output-directory.tgz"
 # 下载轻节点数据
 download() {
   local url=$1
   if type wget >/dev/null 2>&1; then
-    wget -P $2 --no-check-certificate -q $url
+    wget -P $2 --no-check-certificate q $url
   elif type curl >/dev/null 2>&1; then
     curl -o $2 -OLJ $url
   else
     echo 'info: no exists wget or curl, make sure the system can use the "wget" or "curl" command'
   fi
 }
-
 download "$url" "${databaseDir}"
-tar -zxvf LiteFullNode_output-directory.tgz
